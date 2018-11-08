85440b5a366b7ef430d6668cb20613eba19b2fdb
douglasrg1 <douglasrg1@gmail.com>
2018-11-08 18:32:05 -0200

    tela de cadastro de movimentacao quae terminada

--- Gestao/Controllers/MovimentaEstoquesController.cs
+++ Gestao/Controllers/MovimentaEstoquesController.cs
@@ -64,6 +64,8 @@ namespace Gestao.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create( MovimentaEstoque movimentaEstoque)
         {
+            var dados = dadosRecebidos();
+
             if (ModelState.IsValid)
             {
                 db.movimentaEstoque.Add(movimentaEstoque);
@@ -136,6 +138,50 @@ namespace Gestao.Controllers
             var valorProduto = db.Produto.Where(p => p.id == id).First().valorVenda;
             return valorProduto;
         }
+        public dadosRecebidosMovimentacao dadosRecebidos()
+        {
+            dadosRecebidosMovimentacao dadosRecebidosmov = new dadosRecebidosMovimentacao();
+            dadosMovimentacao dadosMovimentacao = new dadosMovimentacao();
+            produtosMovimentacao produtosMovimentacao = new produtosMovimentacao();
+
+            //dados movimentacao
+            dadosMovimentacao.idFornecedor = Convert.ToInt32(Request["dadosRecebidos[dadosMovimentacao][idForncedor]"]);
+            dadosMovimentacao.numeroDocumento = Request["dadosRecebidos[dadosMovimentacao][numeroDocumento]"];
+            dadosMovimentacao.totalMovimentacao = Convert.ToDecimal(Request["dadosRecebidos[dadosMovimentacao][totalMovimentacao]"]);
+            dadosMovimentacao.valorFrete = Convert.ToDecimal(Request["dadosRecebidos[dadosMovimentacao][valorFrete]"]);
+            dadosMovimentacao.tipoMovimentacao = Request["dadosRecebidos[dadosMovimentacao][tipoMovimentacao]"];
+            dadosRecebidosmov.dadosMovimentacao = dadosMovimentacao;
+            //dados produtos
+            var produtoMov = this.Request.Form.AllKeys.Where(k => k.Contains("dadosRecebidos[produtosMovimentacao]")).ToList();
+            var quantKeys = (produtoMov.Count) / 10;
+
+            if (quantKeys == 0)
+                return dadosRecebidosmov;
+
+            dadosRecebidosmov.produtosMovimentacao = new List<produtosMovimentacao>();
+
+            for (var i = 0; i < quantKeys; i++)
+            {
+                produtosMovimentacao.idproduto = Convert.ToInt32(Request["dadosRecebidos[produtosMovimentacao][" + i + "][idproduto]"]);
+                produtosMovimentacao.valorUnitario = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][valorUnitario]"]);
+                produtosMovimentacao.quantidade = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][quantidade]"]);
+                produtosMovimentacao.porcDesconto = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][porcDesconto]"]);
+                produtosMovimentacao.valorDesconto = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][valorDesconto]"]);
+                produtosMovimentacao.valorTotal = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][valorTotal]"]);
+                produtosMovimentacao.obs = Request["dadosRecebidos[produtosMovimentacao][" + i + "][obs]"];
+                produtosMovimentacao.valorIpi = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][valorIpi]"]);
+                produtosMovimentacao.valorIcms = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][valorIcms]"]);
+                produtosMovimentacao.Cfop = Convert.ToDecimal(Request["dadosRecebidos[produtosMovimentacao][" + i + "][cfop]"]);
+
+                dadosRecebidosmov.produtosMovimentacao.Add(produtosMovimentacao);
+
+            }
+
+
+
+
+            return dadosRecebidosmov;
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -146,4 +192,35 @@ namespace Gestao.Controllers
             base.Dispose(disposing);
         }
     }
+}
+
+public struct dadosMovimentacao
+{
+    public decimal totalMovimentacao { get; set; }
+    public string numeroDocumento { get; set; }
+    public decimal valorFrete { get; set; }
+    public int idFornecedor { get; set; }
+    public string tipoMovimentacao { get; set; }
+
+
+
+}
+public struct produtosMovimentacao
+{
+    public int idproduto { get; set; }
+    public string obs { get; set; }
+    public decimal porcDesconto { get; set; }
+    public decimal quantidade { get; set; }
+    public decimal valorDesconto { get; set; }
+    public decimal valorTotal { get; set; }
+    public decimal valorUnitario { get; set; }
+    public decimal valorIcms { get; set; }
+    public decimal valorIpi { get; set; }
+    public decimal Cfop { get; set; }
+
+}
+public struct dadosRecebidosMovimentacao
+{
+    public dadosMovimentacao dadosMovimentacao;
+    public List<produtosMovimentacao> produtosMovimentacao;
 }
